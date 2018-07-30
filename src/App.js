32fbbad35f8d46f1f778672e029b18ede8233c18
Wolfgang Becker <wolli.becker@gmail.com>
2018-07-30 22:08:04 +0200

    Create basic navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+
 import { handleInitialData } from './actions/shared';
+import './App.css';
+
+import { Home } from './Home';
+import { NewQuestion } from './NewQuestion';
+import { Leaderboard } from './Leaderboard';
+import { Login } from './Login';
 
 class App extends Component {
   componentDidMount() {
@@ -9,7 +18,33 @@ class App extends Component {
 
   render() {
     return (
-      <div></div>
+      <BrowserRouter>
+        <div>
+          <Menu>
+            <Menu.Item name='home' as={Link} to='/'>
+              Home
+            </Menu.Item>
+
+            <Menu.Item name='submit' as={Link} to='/add'>
+              Ask Question
+            </Menu.Item>
+
+            <Menu.Item name='submit' as={Link} to='/leaderboard'>
+              Leaderboard
+            </Menu.Item>
+
+            <Menu.Menu position='right'>
+              <Menu.Item name='login' as={Link} to='/login'>
+                Login
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/add" component={NewQuestion}/>
+          <Route exact path="/leaderboard" component={Leaderboard}/>
+          <Route exact path="/login" component={Login} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/NewQuestion.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class NewQuestion extends Component {
+  render() {
+    return <div>NewQuestion</div>;
+  }
+}
