985a22f4d168b984875ae046286ca604ab7e980b
Wolfgang Becker <wolli.becker@gmail.com>
2018-08-03 00:00:51 +0200

    Create NewQuestion form layout

--- src/components/question/NewQuestion.js
+++ src/components/question/NewQuestion.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import { Card, Button, Form } from 'semantic-ui-react'
 
 export class NewQuestion extends Component {
   render() {
-    return <div>NewQuestion</div>;
+    return (
+      <Card>
+        <Card.Content header='Would you rather...' />
+        <Card.Content>
+          <Form>
+            <Form.Field>
+              <input placeholder='do this' />
+            </Form.Field>
+            or
+            <Form.Field>
+              <input placeholder='do that' />
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        </Card.Content>
+      </Card>
+    );
   }
 }
