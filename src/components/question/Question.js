e5c9cec1fd9211e8696957762aa8c0785df5c7bd
Wolfgang Becker <wolli.becker@gmail.com>
2018-08-11 19:06:48 +0200

    Refactor Question component

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, withRouter } from 'react-router-dom';
 import { Button, Card, Image, Progress } from 'semantic-ui-react';
 
-import { handleAnswerQuestion } from '../../actions/shared';
+import { handleAnswerQuestion } from 'actions/shared';
 
 class Question extends Component {
   saveQuestionAnswer(answer) {
@@ -15,11 +15,77 @@ class Question extends Component {
     }
   }
 
+  totalVotes = (question) => {
+    return question['optionOne'].votes.length + question['optionTwo'].votes.length;
+  }
+
+  votesInFavor = (question, answer) => {
+    return question[answer].votes.length;
+  }
+
+  unansweredContent = (question) => {
+    return (
+      <Card.Content>
+        <div className='ui two buttons'>
+          <Button
+            basic
+            color='green'
+            onClick={this.saveQuestionAnswer.bind(this, 'optionOne')}>
+            {question.optionOne.text}
+          </Button>
+          <Button.Or />
+          <Button
+            basic
+            color='red'
+            onClick={this.saveQuestionAnswer.bind(this, 'optionTwo')}>
+            {question.optionTwo.text}
+          </Button>
+        </div>
+      </Card.Content>
+    )
+  }
+
+  answeredContent = (question, answer) => {
+    return (
+      <Fragment>
+        <Card.Content>
+          <div className = 'ui two buttons' >
+            <Button
+              basic={!(answer === 'optionOne')}
+              disabled={!(answer === 'optionOne')}
+              color='green'
+              onClick={this.saveQuestionAnswer.bind(this, 'optionOne')}>
+              {question.optionOne.text}
+            </Button>
+            <Button.Or />
+            <Button
+              basic={!(answer === 'optionTwo')}
+              disabled={!(answer === 'optionTwo')}
+              color='red'
+              onClick={this.saveQuestionAnswer.bind(this, 'optionTwo')}>
+              {question.optionTwo.text}
+            </Button>
+          </div>
+        </Card.Content>
+        <Card.Content extra>
+          <Progress
+            value={this.votesInFavor(question, answer)}
+            total={this.totalVotes(question)}
+            color='teal'
+            precision='0'
+            progress='percent'>
+            opted for this answer
+          </Progress>
+        </Card.Content>
+      </Fragment>
+    );
+  }
+
   render () {
     const { authorInfo, question, answer } = this.props;
 
     if (!question) {
-      return <Redirect to='/404'/>
+      return <Redirect to='/404'/>;
     }
 
     return (
@@ -29,54 +95,7 @@ class Question extends Component {
           <Card.Header>Would You Rather...</Card.Header>
           <Card.Meta>{ authorInfo.name }</Card.Meta>
         </Card.Content>
-        <Card.Content>
-          { answer ? (
-            <div className='ui two buttons'>
-              <Button
-                basic={!(answer === 'optionOne')}
-                disabled={!(answer === 'optionOne')}
-                color='green'
-                onClick={this.saveQuestionAnswer.bind(this, 'optionOne')}>
-                {question.optionOne.text}
-              </Button>
-              <Button.Or />
-              <Button
-                basic={!(answer === 'optionTwo')}
-                disabled={!(answer === 'optionTwo')}
-                color='red'
-                onClick={this.saveQuestionAnswer.bind(this, 'optionTwo')}>
-                {question.optionTwo.text}
-              </Button>
-            </div>
-          ) : (
-              <div className='ui two buttons'>
-                <Button
-                  basic
-                  color='green'
-                  onClick={this.saveQuestionAnswer.bind(this, 'optionOne')}>
-                  {question.optionOne.text}
-                </Button>
-                <Button.Or />
-                <Button
-                  basic
-                  color='red'
-                  onClick={this.saveQuestionAnswer.bind(this, 'optionTwo')}>
-                  {question.optionTwo.text}
-                </Button>
-              </div>
-          ) }
-        </Card.Content>
-        { answer && (
-          <Card.Content extra>
-            <Progress
-              value={question[answer].votes.length}
-              total={question['optionOne'].votes.length + question['optionTwo'].votes.length}
-              color='teal'
-              progress='percent'>
-              opted for this answer
-            </Progress>
-          </Card.Content>
-        )}
+        { answer ? this.answeredContent(question, answer) : this.unansweredContent(question) }
       </Card>
     )
   }
