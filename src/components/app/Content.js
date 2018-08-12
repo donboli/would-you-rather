b9af826e71f2a66d6c1a2da4770e3d1c9cc490cb
Wolfgang Becker <wolli.becker@gmail.com>
2018-08-12 15:14:46 +0200

    Center route content in the middle

--- src/components/app/Content.js
+++ src/components/app/Content.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
 import Home from '../home/Home';
 import { NewQuestion, Question } from '../question';
 import Leaderboard from '../leaderboard/Leaderboard';
 import Login from '../login/Login';
 import { NotFound } from '../errors/NotFound';
+import './Content.css';
 
 export class Content extends Component {
   render () {
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/add" component={NewQuestion} />
-        <Route exact path="/leaderboard" component={Leaderboard} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/questions/:question_id" component={Question} />
-        <Route exact path="/404" component={NotFound} />
-      </Switch>
+      <div className='container'>
+        <Switch>
+          <Grid
+            verticalAlign='middle'
+            centered>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/add" component={NewQuestion} />
+            <Route exact path="/leaderboard" component={Leaderboard} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/questions/:question_id" component={Question} />
+            <Route exact path="/404" component={NotFound} />
+          </Grid>
+        </Switch>
+      </div>
     )
   }
 }
